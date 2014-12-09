6148ba1c947becee56acc11b82f44f59f1f557f3
Parul Gupta <parul.acro@gmail.com>
2014-12-09 18:00:32 +0530

    push Heroku

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,12 +6,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # end
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
+    assert_no_difference 'User.count', 1 do
+      post users_path, user: { name:  "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/show'
+        assert is_logged_in?
+
   end
 end
