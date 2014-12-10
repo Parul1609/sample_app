5417713131702a517437a94e37fefcce3cd4068b
Parul Gupta <parul.acro@gmail.com>
2014-12-10 11:33:06 +0530

    Finish login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ActiveRecord::Base
   end
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+        return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   # Forgets a user.
